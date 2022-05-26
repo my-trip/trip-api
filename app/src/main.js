68e387bf1cff628014c52bc777ed51a90a07ab03
jose <guislima47@gmail.com>
2022-05-25 22:48:51 -0300

    Login screen initial implementation

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createProvider } from './vue-apollo'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/css/main.css'
+
+import routes from '@/router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
---
+++ app/src/router/index.js
@@ -0,0 +1,8 @@
+import Login from '@/pages/login/login'
+
+export default [
+    {
+        path: '/login',
+        component: Login
+    }
+]
